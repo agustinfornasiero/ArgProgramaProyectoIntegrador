2e54124054a88b858be0189f8fa85f9bf1a31f4c
Agustin Fornasiero <75949635+agustinfornasiero@users.noreply.github.com>
2023-03-03 02:25:17 -0300

    Se incorporaron iconos de social media y estilos
    Modificación de Iconos Social Media

--- PortFolioAgustin/src/app/app.module.ts
+++ PortFolioAgustin/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent
+    SocialComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
